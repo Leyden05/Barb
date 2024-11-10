0b0d3e9a479ccfdb35147aebd1af8d6d79b968d5
leyden <fusionsoccer05@gmail.com>
2024-11-10 12:19:11 -0600

    small change to contact

--- barbsite/app/contact/page.jsx
+++ barbsite/app/contact/page.jsx
@@ -11,9 +11,9 @@ export default function Home() {
       <div class="flex flex-col md:flex-row gap-8 md:gap-32 w-full justify-center">
         <ContactFormLarge />
         <div class="mt-8 md:mt-32 max-w-7xl flex flex-col items-center text-white font-light text-lg">
-          <h2 class="text-white font-medium text-lg">Grant Pittman @ pittManagement</h2>
-          <a href="mailto:grantcpittman@gmail.com" class="hover:text-gray-300">grantcpittman@gmail.com</a>
-          <a href="tel:3615490469" class="hover:text-gray-300">361-549-0469</a>
+          <h2 class="text-white font-medium text-lg">Barb</h2>
+          <a href="mailto:barbmusicinquiries@gmail.com" class="hover:text-gray-300">barbmusicinquiries@gmail.com</a>
+          {/* <a href="tel:3615490469" class="hover:text-gray-300">361-549-0469</a> */}
         </div>
       </div>
     </main>
