3207df521c6856fbe3723908eca446a7f6612020
leyden <fusionsoccer05@gmail.com>
2024-11-21 13:16:21 -0600

    fixing image on mobile

--- barbsite/app/components/Shows.jsx
+++ barbsite/app/components/Shows.jsx
@@ -5,7 +5,7 @@ const Shows = () => {
     <div className="-z-[40] w-0 h-0">
       <div className="w-full h-screen">
         <Image
-          className="w-full h-[1100px] object-cover absolute top-[-10.5rem] -z-[40]"
+          className="min-w-[200%] md:min-w-full h-[1100px] object-cover object-center left-[-35%] md:left-0 translate-x-[-30%] md:translate-x-0 absolute top-[-10.5rem] -z-[40]"
           src={require("../assets/barbBG.JPG")}
           alt="my face"
         />
